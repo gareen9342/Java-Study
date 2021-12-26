cb0ade4a49d0120d02c1edaf0f31c5da46173bbe
조가린 <41724184+gareen9342@users.noreply.github.com>
2021-12-26 23:06:07 +0900

    객체지향 - 클래스 메서드, 인스턴스 메서드

--- src/com/oop/Main.java
+++ src/com/oop/Main.java
@@ -1,4 +1,16 @@
 package com.oop;
 
 public class Main {
+  // 반환 값이 없는 메서드에서 연산결과를 넘길 수 있는 방법
+  static int add(int a, int b){
+    return  a+b;
+  }
+
+  static void add(int a, int b, int[] result){
+    result[0] = a+b;
+  }
+
+  public static void main(String[] args) {
+
+  }
 }
---
+++ src/com/oop/ReferenceParamEx2.java
@@ -0,0 +1,17 @@
+package com.oop;
+
+class ReferenceParamEx2 {
+  public static void main(String[] args) {
+    int[] x = {10};
+    System.out.println(x[0]);
+
+    change(x);
+    System.out.println("After change x");
+    System.out.println("main() x[0] =" + x[0]);
+  }
+
+  static void change(int[] x) {
+    x[0] = 1000;
+    System.out.println("change = "+ x[0]);
+  }
+}
